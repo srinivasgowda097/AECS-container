a0a6d178ec41a0f5cc6fd7a8f7657dbe82148051
rosmi <rosmi@cloudboxonline.com>
2014-04-07 13:53:19 +0530

    changed db

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpressdb');
+define('DB_NAME', 'wordpressdb1');
 
 /** MySQL database username */
 define('DB_USER', 'wordpressuser');
