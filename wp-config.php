9af70a71ec13e057cd35765241cc9bea49297061
rosmi <rosmi@cloudboxonline.com>
2014-07-01 16:46:01 +0530

    test

--- wp-config.php
+++ wp-config.php
@@ -19,7 +19,7 @@
 define('DB_NAME', 'wordpressdb');
 
 /** MySQL database username */
-define('DB_USER', 'wordpress1');
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'word!23');
