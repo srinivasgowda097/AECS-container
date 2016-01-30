c4aa9d1a062dfcd86d34291809d0c696ae67c5a7
rosmi <rosmi@cloudmunch.com>
2016-01-30 16:29:44 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'db:3306');
+define('DB_HOST', 'db');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
